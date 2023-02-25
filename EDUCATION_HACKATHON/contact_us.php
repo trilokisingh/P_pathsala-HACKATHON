ddc9babdeb810101e6ad5088fefd931f1e434da0
code_x <73876020+trilokisingh@users.noreply.github.com>
2023-02-25 12:16:13 +0530

    educational website

---
+++ EDUCATION_HACKATHON/contact_us.php
@@ -0,0 +1,33 @@
+<?php
+    $db_hostname="127.0.0.1";
+    $db_username="root";
+    $db_password="";
+    $db_name="student_query";
+
+    $conn=mysqli_connect($db_hostname,$db_username,$db_password,$db_name);
+    if(!$conn)
+    {
+        echo"Connection Failed:",mysqli_connect_error();
+        exit;
+    }
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $name=$_POST['name'];
+        $email=$_POST['email'];
+        $phone=$_POST['phone'];
+        $subject=$_POST['subject'];
+        $message=$_POST['message'];
+   
+   
+
+    $sql="Insert into form_query(name,email,phone,subject,message) values ('$name','$email','$phone','$subject','$message')";
+    $result=mysqli_query($conn,$sql);
+    if(!$result)
+    {
+        echo "error: ",mysqli_error($conn);
+        exit;
+    }
+    echo "We will contact you soon";
+    mysqli_close($conn);
+}
+ 
+?>
